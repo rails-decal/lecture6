1cae3d30c539e25ae30fa66d80332bbf12826aa0
Sam Lau <samlau95@gmail.com>
2014-10-14 20:37:31 -0700

    Add user id

--- test/models/pokemon_test.rb
+++ test/models/pokemon_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: pokemons
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  level       :integer
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#  user_id     :integer
+#
+
 require 'test_helper'
 
 class PokemonTest < ActiveSupport::TestCase
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  num_badges :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
